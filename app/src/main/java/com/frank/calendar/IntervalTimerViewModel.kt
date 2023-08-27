33b5c6e79693bd0e9c22364b4eeacb42e42f51ce
FrankLiQiang <lq.cn@protonmail.ch>
2023-08-27 23:42:31 +0900

    Initial commit

---
+++ app/src/main/java/com/frank/calendar/IntervalTimerViewModel.kt
@@ -0,0 +1,84 @@
+package com.frank.calendar
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
+
+var gongliDate = ""
+var nongliDate = ""
+var newCurrentDate = -1
+var currentDateNum = -2
+var dateColor = true
+var toDate: Calendar = Calendar.getInstance()
+
+fun getNongLiDate(): String {
+    return if (currentDateNum == newCurrentDate) {
+        nongliDate
+    } else {
+        nongliDate = "${nongli()}  ${leftDays()}"
+        nongliDate
+    }
+}
+
+fun getCurrentTime(): String {
+    val localDateTime = LocalDateTime.now()
+    newCurrentDate = localDateTime.dayOfMonth
+    val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+    return localDateTime.format(formatter)
+}
+
+fun getCurrentDate(): String {
+    return if (currentDateNum == newCurrentDate) {
+        gongliDate
+    } else {
+        val wk = nowWeek()
+        dateColor = !wk.startsWith("星期")
+        gongliDate = "${nowDate()}  $wk"
+        currentDateNum = newCurrentDate
+        gongliDate
+    }
+}
+
+val nongli: () -> String = {
+    val calendar: Calendar = Calendar.getInstance()
+    "农历 ${
+        LunarCalendar.getLunarText(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+    }"
+}
+
+val leftDays: () -> String = {
+    val cNow = Calendar.getInstance()
+    cNow.set(Calendar.HOUR_OF_DAY, 0)
+    cNow.set(Calendar.MINUTE, 0)
+    cNow.set(Calendar.SECOND, 0)
+    cNow.set(Calendar.MILLISECOND, 0)
+    val diff = toDate.timeInMillis - cNow.timeInMillis
+    val leftDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS).toInt()
+    if (leftDays > 0) "  还剩 $leftDays 天" else ""
+}
+
+val nowDate: () -> String = {
+    val localDateTime = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日")
+    localDateTime.format(formatter)
+}
+
+val nowWeek: () -> String = {
+    val calendar: Calendar = Calendar.getInstance()
+
+    val solar = LunarCalendar.gregorianFestival(
+        calendar.get(Calendar.MONTH) + 1,
+        calendar.get(Calendar.DAY_OF_MONTH)
+    )
+    solar.ifEmpty {
+        val day: Int = calendar.get(Calendar.DAY_OF_WEEK)
+        val weekString = "日一二三四五六"
+        "星期${weekString.substring(day - 1, day)}"
+    }
+}
+
---
+++ app/src/main/java/com/frank/calendar/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.frank.calendar
+
+import android.app.DatePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.frank.calendar.ui.theme.CalendarTheme
+import java.util.Calendar
+import java.util.Timer
+import java.util.TimerTask
+
+private var thisTimer: Timer = Timer()
+private var thisTask: TimerTask? = null
+var time by mutableStateOf("")
+var leftDate by mutableStateOf("")
+var date by mutableStateOf("")
+var isRed by mutableStateOf(false)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContent {
+            LunarCalendar.init(this)
+            ReadToDate()
+
+            thisTask = object : TimerTask() {
+                override fun run() {
+                    try {
+                        time = getCurrentTime()
+                        leftDate = getNongLiDate()
+                        date = getCurrentDate()
+                        isRed = dateColor
+                    } catch (e: Exception) {
+                        e.toString()
+                    }
+                }
+            }
+            thisTimer.scheduleAtFixedRate(thisTask, 0, 1000)
+            CalendarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black),
+                ) {
+                    MainUI(::leftTimeClicked)
+                }
+            }
+        }
+    }
+
+    fun leftTimeClicked() {
+        val year = toDate.get(Calendar.YEAR)
+        val month = toDate.get(Calendar.MONTH)
+        val day = toDate.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            { _, year2, monthOfYear, dayOfMonth ->
+                toDate.set(year2, monthOfYear, dayOfMonth, 0, 0, 0)
+                toDate.set(Calendar.MILLISECOND, 0)
+                SaveTimePerSet()
+                currentDateNum = -1
+            },
+            year,
+            month,
+            day
+        )
+        dpd.show()
+    }
+
+    private fun SaveTimePerSet() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putLong(getString(R.string.prefs_timePerWorkSet), toDate.timeInMillis)
+            commit()
+        }
+    }
+
+    private fun ReadToDate() {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val defaultValue = Calendar.getInstance().timeInMillis
+        val highScore = sharedPref.getLong(getString(R.string.prefs_timePerWorkSet), defaultValue)
+        toDate.timeInMillis = highScore
+    }
+}
+
+@Composable
+fun MainUI(event: () -> Unit, modifier: Modifier = Modifier) {
+    Column(Modifier.background(Black), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = time,
+            fontSize = 160.sp,   //130.sp,
+            color = DarkGray,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(1f, true)
+        )
+        Text(
+            text = leftDate,
+            fontSize = 50.sp,       //40.sp,
+            color = DarkGray,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.clickable { event() }
+        )
+        Text(
+            text = date,
+            fontSize = 60.sp,       //50.sp,
+            color = if (isRed) Red else DarkGray,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalendarPreview() {
+    CalendarTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainUI({ isRed = true })
+        }
+    }
+}
