3c93accce26dc6a088fdb3298b7f4aee2fab02e9
= <=>
2023-12-08 15:04:26 +0900

    auto fit device

--- app/src/main/java/com/frank/calendar/MainActivity.kt
+++ app/src/main/java/com/frank/calendar/MainActivity.kt
@@ -16,17 +16,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.DarkGray
-import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.frank.calendar.ui.theme.CalendarTheme
 import java.util.Calendar
@@ -35,9 +34,10 @@ import java.util.TimerTask
 
 private var thisTimer: Timer = Timer()
 private var thisTask: TimerTask? = null
-var time by mutableStateOf("")
-var leftDate by mutableStateOf("")
-var date by mutableStateOf("")
+var textColor by mutableStateOf(DarkGray)
+var time by mutableStateOf("09:35:23")
+var leftDate by mutableStateOf("农历十月廿六")
+var date by mutableStateOf("2023年12月08日")
 var isRed by mutableStateOf(false)
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             LunarCalendar.init(this)
-            ReadToDate()
+            readToDate()
 
             thisTask = object : TimerTask() {
                 override fun run() {
@@ -74,7 +74,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun leftTimeClicked() {
+    private fun leftTimeClicked() {
         val year = toDate.get(Calendar.YEAR)
         val month = toDate.get(Calendar.MONTH)
         val day = toDate.get(Calendar.DAY_OF_MONTH)
@@ -84,7 +84,7 @@ class MainActivity : ComponentActivity() {
             { _, year2, monthOfYear, dayOfMonth ->
                 toDate.set(year2, monthOfYear, dayOfMonth, 0, 0, 0)
                 toDate.set(Calendar.MILLISECOND, 0)
-                SaveTimePerSet()
+                saveTimePerSet()
                 currentDateNum = -1
             },
             year,
@@ -94,7 +94,7 @@ class MainActivity : ComponentActivity() {
         dpd.show()
     }
 
-    private fun SaveTimePerSet() {
+    private fun saveTimePerSet() {
         val sharedPref = getPreferences(Context.MODE_PRIVATE)
         with(sharedPref.edit()) {
             putLong(getString(R.string.prefs_timePerWorkSet), toDate.timeInMillis)
@@ -102,7 +102,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun ReadToDate() {
+    private fun readToDate() {
         val sharedPref = getPreferences(Context.MODE_PRIVATE)
         val defaultValue = Calendar.getInstance().timeInMillis
         val highScore = sharedPref.getLong(getString(R.string.prefs_timePerWorkSet), defaultValue)
@@ -112,38 +112,76 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainUI(event: () -> Unit, modifier: Modifier = Modifier) {
+
+    val minTextSize = 12.sp
+    val maxTextSize = 212.sp
+    var textSize by remember("") { mutableStateOf(maxTextSize) }
+    var textSize2 by remember("") { mutableStateOf(maxTextSize) }
+    var textSize3 by remember("") { mutableStateOf(maxTextSize) }
     Column(Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
             text = time,
-            fontSize = 130.sp,   //160.sp,
-            //color = DarkGray,
-            color = Color(0xFF018786),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            onTextLayout = {
+                if (it.hasVisualOverflow && textSize > minTextSize) {
+                    textSize = (textSize.value - 1.0F).sp
+                }
+            },
+            fontSize = textSize,
+            color = textColor,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.weight(1f, true)
+            modifier = Modifier
+                .weight(1.2f, true)
+                .clickable {
+                    textColor = if (textColor == DarkGray) {
+                        Color(0xFF018786)
+                    } else {
+                        DarkGray
+                    }
+                }
         )
         Text(
             text = leftDate,
-            fontSize = 40.sp,       //50.sp,
-            //color = DarkGray,
-            color = Color(0xFF018786),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            onTextLayout = {
+                if (it.hasVisualOverflow && textSize2 > minTextSize) {
+                    textSize2 = (textSize2.value - 1.0F).sp
+                }
+            },
+            fontSize = textSize2,
+            color = textColor,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.clickable { event() }
+            modifier = Modifier
+                .clickable { event() }
+                .weight(0.3f, true)
         )
         Text(
             text = date,
-            fontSize = 50.sp,       //60.sp,
-            //color = if (isRed) Red else DarkGray,
-            color = if (isRed) Color(0xFFBB86FC) else Color(0xFF018786),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            onTextLayout = {
+                if (it.hasVisualOverflow && textSize3 > minTextSize) {
+                    textSize3 = (textSize3.value - 1.0F).sp
+                }
+            },
+            fontSize = textSize3,
+            color = if (isRed) Color(0xFFBB86FC) else textColor,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
-            modifier = modifier
+            modifier = modifier.weight(0.5f, true)
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 2000,
+    heightDp = 900,
+)
 @Composable
 fun CalendarPreview() {
     CalendarTheme {
@@ -155,3 +193,4 @@ fun CalendarPreview() {
         }
     }
 }
+
