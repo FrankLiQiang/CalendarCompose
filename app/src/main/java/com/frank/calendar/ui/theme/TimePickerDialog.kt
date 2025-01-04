7dda371d23e27029da97972425c740513281c345
李強 <30677343+FrankLiQiang@users.noreply.github.com>
2025-01-04 18:21:00 +0900

    去掉滑块选择时辰

--- app/src/main/java/com/frank/calendar/ui/theme/TimePickerDialog.kt
+++ app/src/main/java/com/frank/calendar/ui/theme/TimePickerDialog.kt
@@ -8,30 +8,34 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Slider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.frank.calendar.LunarCalendar
 import com.frank.calendar.LunarUtil
 import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
@@ -39,13 +43,34 @@ import java.time.format.DateTimeFormatter
 var isShowSettingDialog by mutableStateOf(false)
 var isLunar by mutableStateOf(false)
 var isLeap by mutableStateOf(false)
-var theTime = LocalDateTime.now()
-var chooseTime by mutableStateOf(theTime.hour)
+var theTime: LocalDateTime = LocalDateTime.now()
+var chooseTime by mutableIntStateOf(theTime.hour)
 var sTB by mutableStateOf("")
 var sYearName by mutableStateOf("")
 var dateInfo by mutableStateOf("")
 var oldTimeStamp = 0L
 var oldChooseTime = 0
+
+
+//val menuItemData = List(100) { "Option ${it + 1}" }
+val menuItemData = arrayOf(
+    "子 23:00〜01:00",
+    "丑 01:00〜03:00",
+    "寅 03:00〜05:00",
+    "卯 05:00〜07:00",
+    "辰 07:00〜09:00",
+    "巳 09:00〜11:00",
+    "午 11:00〜13:00",
+    "未 13:00〜15:00",
+    "申 15:00〜17:00",
+    "酉 17:00〜19:00",
+    "戌 19:00〜21:00",
+    "亥 21:00〜23:00"
+)
+var expanded by mutableStateOf(false)
+var chosenDate by mutableStateOf(theTime.toLocalDate())
+@OptIn(ExperimentalMaterial3Api::class)
+lateinit var datePickerState: DatePickerState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -67,7 +92,7 @@ private fun ShowSettingDialog(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                val datePickerState =
+                datePickerState =
                     rememberDatePickerState(
                         initialSelectedDateMillis = theTime.toLocalDate()
                             .atStartOfDay(ZoneOffset.ofHours(0)).toInstant().toEpochMilli()
@@ -77,24 +102,8 @@ private fun ShowSettingDialog(
                     DatePicker(
                         title = null,
                         state = datePickerState,
+                        showModeToggle = false,
                     )
-                    Row(
-                        horizontalArrangement = Arrangement.End,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        TextButton(
-                            modifier = Modifier
-                                .background(Color.Black),
-                            onClick = {
-                            }) {
-                            Text(
-                                text = "        ",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 30.sp
-                            )
-                        }
-                    }
                 }
                 Row(
                     horizontalArrangement = Arrangement.Center,
@@ -102,6 +111,11 @@ private fun ShowSettingDialog(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
 
+                    IconButton(onClick = {
+                        datePickerState.setSelection(LocalDateTime.now().toEpochSecond(ZoneOffset.ofHours(9)) * 1000)
+                    }) {
+                        Icon(Icons.Default.DateRange, contentDescription = "More options")
+                    }
                     Checkbox(
                         checked = isLunar, onCheckedChange = {
                             isLunar = !isLunar
@@ -123,6 +137,26 @@ private fun ShowSettingDialog(
                             modifier = Modifier.align(Alignment.CenterVertically)
                         )
                     }
+                    IconButton(onClick = {
+                        expanded = !expanded
+                    }) {
+                        Icon(Icons.Default.Menu, contentDescription = "More options")
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        menuItemData.forEachIndexed { index, value ->
+                            DropdownMenuItem(
+                                text = { Text(value) },
+                                onClick = {
+                                    chooseTime = index * 2
+                                    getDateInfo(datePickerState.selectedDateMillis, chooseTime)
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
                 }
                 Row(
                     horizontalArrangement = Arrangement.Center,
@@ -131,7 +165,7 @@ private fun ShowSettingDialog(
                 ) {
                     Text(
                         text = sTB,
-                        fontSize = 30.sp
+                        fontSize = 20.sp
                     )
                 }
                 Row(
@@ -141,7 +175,7 @@ private fun ShowSettingDialog(
                 ) {
                     Text(
                         text = dateInfo,
-                        fontSize = 30.sp
+                        fontSize = 20.sp
                     )
                 }
                 Row(
@@ -151,54 +185,8 @@ private fun ShowSettingDialog(
                 ) {
                     Text(
                         text = sYearName,
-                        fontSize = 30.sp
+                        fontSize = 20.sp
                     )
-                }
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 18.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "${chooseTime} 时",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 30.sp
-                    )
-                }
-                Slider(
-                    modifier = Modifier.padding(18.dp),
-                    value = chooseTime.toFloat(),
-                    onValueChange = {
-                        chooseTime = it.toInt()
-                        getDateInfo(datePickerState.selectedDateMillis, chooseTime)
-                    },
-                    valueRange = 0f..23f,
-                )
-                Row(
-                    modifier = Modifier.weight(1.0f)
-                ) {}
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(Color.DarkGray)
-                ) {}
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Row(modifier = Modifier.weight(1.0f)) {}
-                    TextButton(onClick = onNegativeClick) {
-                        Text(
-                            text = "Close",
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp
-                        )
-                    }
                 }
             }
         }
